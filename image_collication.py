7f5b46ea40319a24875a4f4ab5a204dcca8ed23e
MaxAndroid33 <yuosifmax309@gmail.com>
2022-11-22 23:18:42 +0300

    enable Capture Images

--- image_collication.py
+++ image_collication.py
@@ -24,26 +24,26 @@ for label in labels:
 
 
 
-# # 4. Capture Images
-# for label in labels:
-#     cap = cv2.VideoCapture(0)
-#     print('Collecting images for {}'.format(label))
-#     time.sleep(5)
-#     for imgnum in range(number_imgs):
-#         print('Collecting image {}'.format(imgnum))
-#         imgname = os.path.join(IMAGES_PATH,label,label+'.'+'{}.jpg'.format(str(uuid.uuid1())))    
-#         while(1):
-#             ret, frame = cap.read()
-#             cv2.imshow('frame', frame)
-#             if cv2.waitKey(1) & 0xFF == ord('s'):
-#                 break
-#         cv2.imwrite(imgname, frame)        
-#         time.sleep(2)
+# 4. Capture Images
+for label in labels:
+    cap = cv2.VideoCapture(0)
+    print('Collecting images for {}'.format(label))
+    time.sleep(5)
+    for imgnum in range(number_imgs):
+        print('Collecting image {}'.format(imgnum))
+        imgname = os.path.join(IMAGES_PATH,label,label+'.'+'{}.jpg'.format(str(uuid.uuid1())))    
+        while(1):
+            ret, frame = cap.read()
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('s'):
+                break
+        cv2.imwrite(imgname, frame)        
+        time.sleep(2)
 
-#         if cv2.waitKey(1) & 0xFF == ord('q'):
-#             break
-# cap.release()
-# cv2.destroyAllWindows()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
 
 
 # 5. Image Labelling
